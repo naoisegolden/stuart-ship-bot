b22b3964778ac6a0ff7f3ea71862490b184edd8e
Naoise Golden <naoise.golden@gmail.com>
2020-03-25 00:50:10 +0100

    Basic Slack bot functionality

--- api/talk.js
+++ api/talk.js
@@ -1,3 +1,38 @@
+const { WebClient } = require('@slack/web-api');
+
+const token = process.env.SLACK_TOKEN;
+
+const web = new WebClient(token);
+
 export default (req, res) => {
-  res.json({ body: req.body });
+  res.status(200);
+
+  if (!req.body || !req.body.event) {
+    res.send();
+    return;
+  }
+
+  const {
+    type,
+    challenge,
+    channel,
+    text: input,
+  } = req.body.event;
+
+  if (type === 'url_verification' && challenge) {
+    console.log('Callenge received');
+    res.json({ challenge });
+  }
+
+  if (type === 'app_mention') {
+    let text = 'All aboard!';
+
+    if (input.includes('help')) {
+      text = 'All Hands on Deck!'
+    }
+
+    web.chat
+      .postMessage({ text, channel })
+      .finally(() => { res.send(); });
+  }
 };
